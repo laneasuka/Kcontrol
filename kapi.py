614d0aac81dc97058ba55f74b3606efcc6f879f1
laneasuka <laneasuka@gmail.com>
2015-09-09 17:46:43 +0800

    Get code,access_token,refresh_token,userid OK.

--- kapi.py
+++ kapi.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import requests
 import json
+
+callbackurl = 'http://www.baidu.com'
+h = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36'}
 
 
 def load(filename='dat'):
@@ -20,45 +24,47 @@ def save(d, filename='dat'):
         f.write(json.dumps(args))
 
 
+def update_token(p):
+    s = requests.Session()  # 会话形式访问，保存cookie
+    r = s.get(p['url_code'] + 'http://www.baidu.com')
+    mc = re.findall(r'"mobilecode".+"(\w{16})"', r.text)[0]
+
+    data = {
+        'username': p['username'],
+        'password': p['password'],
+        'clientid': p['client_id'],
+        'mobilecode': mc
+    }
+    r = s.post(p['url_code'] + 'http://www.baidu.com', data=data)
+    code = r.url.split("=")[1]
+
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': p['client_id'],
+        'client_secret': p['client_secret'],
+        'code': code,
+        'redirect_uri': 'http://www.baidu.com'
+    }
+    r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/accessToken', data=data)
+    token = eval(r.text)
+
+    headers = {'Authorization': 'Bearer %s' % token['access_token']}
+    r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/UserInfo', headers=headers)
+    userinfo = eval(r.text)
+
+    return dict(mobilecode=mc, code=code, access_token=token['access_token'], refresh_token=token['refresh_token'],
+                userid=userinfo['userid'])
+
+
+# save(update_token(load()))
 P = load()
-# print args
-s = requests.Session()  # 会话形式访问，保存cookie
+for k,v in P.items():
+    print k, ' : ', v
 
-data = {
-    'username': P['username'],
-    'password': P['password'],
-    'clientid': P['client_id'],
-    'mobilecode': '2ac4OpVXgT0yllHM'
+hds = {'Authorization': 'Bearer %s' % P['access_token']}
+dat = {
+    'userid': P['userid']
 }
+# req = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/User/getGeneralRemoteList', headers=hds, data=dat)
 
-# r = s.get(args['url_code'])
-# print r.content
-r = s.post(P['url_code'], data=data)
-# print r.content, '\n', r.url, '\n', r.status_code, '\n', r.history
-
-code = r.url.split("=")[1]
-print 'code:          ', code
-
-data = {
-    'grant_type': 'authorization_code',
-    'client_id': P['client_id'],
-    'client_secret': P['client_secret'],
-    'code': code,
-    'redirect_uri': P['callbackurl']
-}
-
-r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/accessToken', data=data)
-token = eval(r.text)
-print 'access_token:  ', token['access_token']
-print 'refresh_token: ', token['refresh_token']
-
-headers = {'Authorization': 'Bearer %s' % token['access_token']}
-r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/UserInfo', headers=headers)
-userinfo = eval(r.text)
-print 'userid:        ', userinfo['userid']
-
-data = {
-    'userid': userinfo['userid']
-}
-r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/User/getGeneralRemoteList', headers=headers, data=data)
-print r.content.decode('utf-8')
+# print req.content.decode('utf-8'), '\n', req.url, '\n', req.status_code, '\n', req.history
