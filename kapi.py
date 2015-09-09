5f994b693c0d6776fe6bf65a231189b431c3d954
laneasuka <laneasuka@gmail.com>
2015-09-09 22:44:39 +0800

    Access API ok!

--- kapi.py
+++ kapi.py
@@ -5,8 +5,19 @@ import os
 import re
 import requests
 import json
+import pprint
 
 callbackurl = 'http://www.baidu.com'
+url_klist = 'http://kk.bigk2.com:8080/KOAuthDemeter/User/getKList'
+url_grlist = 'http://kk.bigk2.com:8080/KOAuthDemeter/User/getGeneralRemoteList'
+url_sendgr = 'http://kk.bigk2.com:8080/KOAuthDemeter/KControl/sendGeneralRemoteOrder'
+url_airlist = 'http://kk.bigk2.com:8080/KOAuthDemeter/User/getAirConditionerRemoteList'
+url_sendair = 'http://kk.bigk2.com:8080/KOAuthDemeter/KControl/sendAirConditionerOrder'
+url_switchk = 'http://kk.bigk2.com:8080/KOAuthDemeter/KControl/doSwitchK'
+url_klight = 'http://kk.bigk2.com:8080/KOAuthDemeter/User/switchKLight'
+url_kstate = 'http://kk.bigk2.com:8080/KOAuthDemeter/Kinfo/getKState'
+url_kinfo = 'http://kk.bigk2.com:8080/KOAuthDemeter/User/getKInfo'
+
 h = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36'}
 
 
@@ -46,11 +57,11 @@ def update_token(p):
         'redirect_uri': 'http://www.baidu.com'
     }
     r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/accessToken', data=data)
-    token = eval(r.text)
+    token = r.json()
 
     headers = {'Authorization': 'Bearer %s' % token['access_token']}
     r = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/UserInfo', headers=headers)
-    userinfo = eval(r.text)
+    userinfo = r.json()
 
     return dict(mobilecode=mc, code=code, access_token=token['access_token'], refresh_token=token['refresh_token'],
                 userid=userinfo['userid'])
@@ -58,13 +69,22 @@ def update_token(p):
 
 # save(update_token(load()))
 P = load()
-for k,v in P.items():
-    print k, ' : ', v
 
-hds = {'Authorization': 'Bearer %s' % P['access_token']}
+hds = {
+    'Authorization': 'Bearer %s' % P['access_token'],
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Accept-Encoding': 'identity'
+}
 dat = {
     'userid': P['userid']
 }
-# req = requests.post('http://kk.bigk2.com:8080/KOAuthDemeter/User/getGeneralRemoteList', headers=hds, data=dat)
-
-# print req.content.decode('utf-8'), '\n', req.url, '\n', req.status_code, '\n', req.history
+req = requests.post(url_grlist, headers=hds, data=json.dumps(dat))
+if req.status_code == 200:
+    rst = req.json()
+    if rst['result'] == 0:
+        pprint.pprint(req.json())
+    else:
+        save(update_token(load()))
+        P = load()
+print req.content.decode('utf-8')  # , '\n', req.url, '\n', req.status_code, '\n', req.history, '\n', req.json()
